943a9a14f45cc2414cc0ba36fb89d52d9e77cf6f
Federico Milano <fmilano@gmail.com>
2021-10-03 09:18:43 +0200

    Improved coding style.

--- Euler1.asm
+++ Euler1.asm
@@ -2,9 +2,9 @@
 ;       Euler1.asm  -- Solution to Project Euler problem 1
 ;
 ; 	Find the sum of all the multiples of 3 or 5 below 1000.
-        NAM	Euler1.asm
-        TTL	Project Euler problem 1
-        OPT	NOG
+                NAM	Euler1.asm
+                TTL	Project Euler problem 1
+                OPT	NOG
 
 PUTLIN		EQU	$B99C		; PUT A LINE	Msg in X-1
 PUTCR		EQU	$B958		; Put CR $0D
@@ -22,20 +22,20 @@ div_by_3        LDX	#3		; GET DIVISOR
                                         ; yes, add to the accumulator
                 LDD     ,S              
                 LDX     #accum
-                JSR     Add24           ; Add 
+                JSR     ADD16_24        ; Add 
                 BRA     next_number
                 
 div_by_5        LDY     ,S              
-                LDX	#5		; GET DIVISOR
-                PSHS	X,Y		; SAVE PARAMETERS IN STACK
-                JSR	UREM16		; UNSIGNED DIVIDE, RETURN REMAINDER 
-                PULS	D		; GET remainder
+                LDX	#5		; get divisor
+                PSHS	X,Y		; save parameters in stack
+                JSR	UREM16		; unsigned divide, return remainder 
+                PULS	D		; get remainder
                 TSTB                    ; is divisible?
                 BNE     next_number     ; no
                 ; Add multiple
                 LDD     ,S              
                 LDX     #accum
-                JSR     Add24           ; Add 
+                JSR     ADD16_24        ; Add 
 next_number     LDY     ,S
                 LEAY    -1,Y
                 BEQ     finish
@@ -59,9 +59,7 @@ accum		ZMB	3	; 24 bit number accumulated result
 resultStr	FCN	"0000000"	; place to store the number as a string string
 
 
-;	Title:			16-bit division	
-;
-;	Name:			SDIV16, UDIV16, SREM16, UREM16
+;	Name:			UREM16
 ;	Purpose:
 ;				UREM16	Divide 2 unsigned 16-bit words and return a 16-bit unsigned remainder
 ;
@@ -91,85 +89,80 @@ resultStr	FCN	"0000000"	; place to store the number as a string string
 ;
 ;	Registers Used:		A,B,CC,X,Y
 ;
-;	Time:			Approximately 955 cycles
+; Based on the implementation by Lance A. Leventhal
 ;
-;	Size:
-;				Program	145 bytes
-;				Data	  3 stack bytes
+UREM16
 ; 
-; UNSIGNED 16-BIT DIVISION, RETURNS REMAINDER
+; check for zero divisor
+; exit, indicating error, if found
+; 
+                LEAX	2,S		; point to divisor 
+                LDD	 ,X		; test divisor
+                BNE	strtdv		; branch if divisor not zero
+                STD	2,X		; divisor is zero, so make result zero 
+                BRA	exitdv		; exit indicating error
 ;
-UREM16:
-; UNSIGNED DIVISION, INDICATE QUOTIENT, REMAINDER BOTH POSITIVE
+; divide unsigned dividend by unsigned divisor 
+; memory addresses hold both dividend and quotient.
+; each time we shift the dividend one bit left,
+; we also shift a bit of the
+; quotient in from the carry at the far right
+; at the end, the quotient has replaced the dividend in memory
+; and the remainder is left in register d
 ; 
-; CHECK FOR ZERO DIVISOR
-; EXIT, INDICATING ERROR, IF FOUND
-; 
-        LEAX	2,S		; POINT TO DIVISOR 
-        LDD	 ,X		; TEST DIVISOR
-        BNE	STRTDV		; BRANCH IF DIVISOR NOT ZERO
-        STD	2,X		; DIVISOR IS ZERO, SO MAKE RESULT ZERO 
-;--	SEC			; INDICATE DIVIDE BY ZERO ERROR
-        BRA	EXITDV		; EXIT INDICATING ERROR
+strtdv          LDD	#0		; extend dividend to 32 bits with 0 
+                LDY	#16		; bit count = 16 
 ;
-; DIVIDE UNSIGNED DIVIDEND BY UNSIGNED DIVISOR 
-; MEMORY ADDRESSES HOLD BOTH DIVIDEND AND QUOTIENT.
-; EACH TIME WE SHIFT THE DIVIDEND ONE BIT LEFT,
-; WE ALSO SHIFT A BIT OF THE
-; QUOTIENT IN FROM THE CARRY AT THE FAR RIGHT
-; AT THE END, THE QUOTIENT HAS REPLACED THE DIVIDEND IN MEMORY
-; AND THE REMAINDER IS LEFT IN REGISTER D
+; shift dividend left with entering at far right 
 ; 
-STRTDV: LDD	#0		; EXTEND DIVIDEND TO 32 BITS WITH 0 
-        LDY	#16		; BIT COUNT = 16 
+div16
+                ROL	3,X		; shift low  byte of dividend 
+                ROL	2,X		; shift next byte of dividend 
+                ROLB			; shift next byte of dividend 
+                ROLA			; shift high byte of dividend
+; 
+; do a trial subtraction of divisor from dividend
+; if difference is non-negative, perform actual subtraction.
+; if difference is negative, continue.
 ;
-; SHIFT DIVIDEND LEFT WITH ENTERING AT FAR RIGHT 
+                CMPD	,X		; trial subtraction of divisor
+                BCS	deccnt		; branch if subtraction fails
+                SUBD	,X		; trial subtraction succeeded,
+                                        ; so subtract divisor from
+                                        ; dividend
 ; 
-DIV16:
-        ROL	3,X		; SHIFT LOW  BYTE OF DIVIDEND 
-        ROL	2,X		; SHIFT NEXT BYTE OF DIVIDEND 
-        ROLB			; SHIFT NEXT BYTE OF DIVIDEND 
-        ROLA			; SHIFT HIGH BYTE OF DIVIDEND
+; update bit counter
+; continue through 16 bits
 ; 
-; DO A TRIAL SUBTRACTION OF DIVISOR FROM DIVIDEND
-; IF DIFFERENCE IS NON-NEGATIVE, PERFORM ACTUAL SUBTRACTION.
-; IF DIFFERENCE IS NEGATIVE, continue.
-;
-        CMPD	,X		; TRIAL SUBTRACTION OF DIVISOR
-        BCS	DECCNT		; BRANCH IF SUBTRACTION FAILS
-        SUBD	,X		; TRIAL SUBTRACTION SUCCEEDED,
-                                ; SO SUBTRACT DIVISOR FROM
-                                ; DIVIDEND
+deccnt          LEAY	-1,Y		; continue until all bits done 
+                BNE	div16
 ; 
-; UPDATE BIT COUNTER
-; CONTINUE THROUGH 16 BITS
+; save remainder in stack
 ; 
-DECCNT: LEAY	-1,Y		; CONTINUE UNTIL ALL BITS DONE 
-        BNE	DIV16
+                STD	4,S
+; remove parameters from stack and exit
 ; 
-; SAVE REMAINDER IN STACK
-; 
-        STD	4,S
-; REMOVE PARAMETERS FROM STACK AND EXIT
-; 
-EXITDV:	LDX	 ,S		; SAVE RETURN ADDRESS 
-        LEAS	4,S		; REMOVE PARAMETERS FROM STACK 
-        JMP	,X		; EXIT TO RETURN ADDRESS
+exitdv	        LDX	 ,S		; save return address 
+                LEAS	4,S		; remove parameters from stack 
+                JMP	,X		; exit to return address
 
-* Subroutine Add24
-*
-* Purpose: MultiPrecAdd adds two multi-byte binary numbers
-*
-* Input: 
-* 	16bit number in D.
-*	Pointer to 24bit number in X.
-* Output: 
-*	24 bit number in X. 
-*
-* Registers affected: A, B, X, Y, U, CC (flags)
-Add24   	ANDCC	#%11111110	; clear carry
 
-                ADCB	2,X		; A = A + byte from second number + carry
+;	Name:			ADD16_24
+;	Purpose:
+;				Add a 16bit number to a 24bit number in big-endian representation
+;
+;	Entry:
+;
+;				Register D: 16bit number
+;                               Register X: Pointer to 24bit number
+;
+;	Exit:
+;
+;				Sum in number pointed by register X 
+;
+ADD16_24   	ANDCC	#%11111110	; clear carry
+
+                ADCB	2,X		; B = B + byte from second number + carry
                 STB	2,X		; store result
 
                 ADCA	1,X		; A = A + byte from second number + carry
